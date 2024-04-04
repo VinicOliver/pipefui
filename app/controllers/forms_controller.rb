c0266c336df96462b650d5c151857aed88ad8813
ViniciusOliver <viniciuscesaroliveira1@gmail.com>
2024-04-04 10:34:36 -0300

    Add Form controller and views

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class FormsController < ApplicationController
+  before_action :set_form, only: %i[edit show update destroy]
+
+  def index
+    @forms = Form.all
+  end
+
+  def new
+    @form = Form.new
+    @form.fields.build
+  end
+
+  def edit; end
+
+  def show; end
+
+  def create
+    @form = Form.new(form_params)
+
+    if @form.save
+      redirect_to root_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @form.update(form_params)
+      redirect_to root_url
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @form.destroy
+
+    redirect_to root_url
+  end
+
+  private
+
+  def set_form
+    @form = Form.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = 'Form not found'
+    redirect_to root_url
+  end
+
+  def form_params
+    params.require(:form).permit(:title, fields_attributes: %i[id data_type label required _destroy])
+  end
+end
