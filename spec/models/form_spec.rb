26904f28327fbefc84c44ae7ea983e95cab25e49
ViniciusOliver <viniciuscesaroliveira1@gmail.com>
2024-04-04 17:22:56 -0300

    Fix with PR suggestions

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Form, type: :model do
-  describe 'validates' do
+  describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
   end
 
